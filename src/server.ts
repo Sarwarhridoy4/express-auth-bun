8a422b129b06e2355ea696dbb46c1aee0bc7eb6f
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-11-11 11:32:13 +0600

    handle gracefull server shutdown

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,81 @@
-import app from './app.js';
-import { PORT } from './config/index.js';
+import type { Server } from "http";
+import http from "http";
+import app from "./app.js";
+import { NODE_ENV, PORT } from "./config/index.js";
+import { prisma } from "./config/db.js";
+
+let server: Server | null = null;
+
+async function connectToDB() {
+  try {
+    await prisma.$connect();
+    console.log("*** DB connection successfull!!");
+  } catch (error) {
+    console.log("*** DB connection failed!");
+    process.exit(1);
+  }
+}
+
+async function startServer() {
+  try {
+    await connectToDB();
+    server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(
+        `🚀 Server is running on${NODE_ENV} environment on port http://localhost:${PORT}`
+      );
+    });
+
+    handleProcessEvents();
+  } catch (error) {
+    console.error("❌ Error during server startup:", error);
+    process.exit(1);
+  }
+}
+
+/**
+ * Gracefully shutdown the server and close database connections.
+ * @param {string} signal - The termination signal received.
+ */
+async function gracefulShutdown(signal: string) {
+  console.warn(`🔄 Received ${signal}, shutting down gracefully...`);
+
+  if (server) {
+    server.close(async () => {
+      console.log("✅ HTTP server closed.");
+
+      try {
+        console.log("Server shutdown complete.");
+      } catch (error) {
+        console.error("❌ Error during shutdown:", error);
+      }
+
+      process.exit(0);
+    });
+  } else {
+    process.exit(0);
+  }
+}
+
+/**
+ * Handle system signals and unexpected errors.
+ */
+function handleProcessEvents() {
+  process.on("SIGTERM", () => gracefulShutdown("SIGTERM"));
+  process.on("SIGINT", () => gracefulShutdown("SIGINT"));
+
+  process.on("uncaughtException", (error) => {
+    console.error("💥 Uncaught Exception:", error);
+    gracefulShutdown("uncaughtException");
+  });
+
+  process.on("unhandledRejection", (reason) => {
+    console.error("💥 Unhandled Rejection:", reason);
+    gracefulShutdown("unhandledRejection");
+  });
+}
+
+// Start the application
+startServer();
 
 
-
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`✅ Server running at http://localhost:${PORT}`);
-});
