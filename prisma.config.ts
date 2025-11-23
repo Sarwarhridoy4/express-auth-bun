97070855208fc0ae2d1bdb55b94d70c1a7e13d9a
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-11-23 11:50:59 +0600

    update prisma driver to 7.0.0

---
+++ prisma.config.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+type Env = {
+  DATABASE_URL: string;
+};
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    // Direct connection for Prisma Migrate
+    url: env<Env>('DATABASE_URL'),
+  },
+});
